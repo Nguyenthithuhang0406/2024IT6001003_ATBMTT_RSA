9019cef4f1f99a8ba695017d8513d2ee962b6b8c
Nguyenthithuhang0406-FE <ntthang.jvb@gmail.com>
2024-11-22 23:03:19 +0700

    RSA

---
+++ src/App.jsx
@@ -0,0 +1,151 @@
+/* eslint-disable */
+import React, { useState } from "react";
+
+import { RSA, ComputeMd5Hash } from "./rsa";
+
+import "./App.scss";
+import FileUploader from "./FileUploader";
+import SaveTextToFile from "./SaveTextToFile";
+const App = () => {
+  const [rsa] = useState(new RSA());
+  const [textToSign, setTextToSign] = useState("");
+  const [signedText, setSignedText] = useState("");
+  const [hashFunction, setHashFunction] = useState("");
+  const [signature, setSignature] = useState("");
+  const [verificationText, setVerificationText] = useState("");
+  const [verificationSignature, setVerificationSignature] = useState("");
+  const [verificationResult, setVerificationResult] = useState("");
+  const [notification, setNotification] = useState("");
+  const [signTextUnChange, setSignTextUnChange] = useState("");
+
+  //hàm ký
+  const handleSign = () => {
+    const hash = ComputeMd5Hash(textToSign);
+    setHashFunction(hash);
+    const signed = rsa.MaHoa(hash);
+    setSignature(signed);
+    setSignedText(textToSign);
+  };
+
+  //hàm chuyển
+  const convert = () => {
+    setVerificationText(textToSign);
+    setVerificationSignature(signature);
+    setVerificationResult(hashFunction);
+    setSignTextUnChange(textToSign);
+  };
+
+  //hàm kiểm tra chữ ký
+  const handleVerify = () => {
+    const hash = ComputeMd5Hash(verificationText);
+    const decryptedHash = rsa.GiaiMa(verificationSignature);
+    if (signTextUnChange !== verificationText) {
+      setNotification("Văn bản ký không hợp lệ");
+    } else {
+      if (hash === decryptedHash && verificationSignature === signature) {
+        setNotification("Chữ ký hợp lệ");
+      } else if (hash !== decryptedHash || verificationSignature !== signature) {
+        setNotification("Chữ ký không hợp lệ");
+      }
+    }
+  };
+
+  return (
+    <div className="app">
+      <div className="digitalSignatures">
+        <p className="tt">CHỮ KÝ SỐ</p>
+        <div className="signature">
+          <div className="generatedSignature">
+            <p className="title">Phát sinh chữ ký</p>
+            <div className="group">
+              <p>Văn bản ký</p>
+              <textarea
+                value={textToSign}
+                onChange={(e) => setTextToSign(e.target.value)}
+              />
+              <FileUploader
+                title="File"
+                onFileChange={(uploadContent) => {
+                  setTextToSign(uploadContent);
+                }}
+              />
+            </div>
+
+            <div className="bt-sign">
+              <button onClick={handleSign}>Ký</button>
+            </div>
+
+            <div className="gr-hashFunction">
+              <p>Hàm băm</p>
+              <textarea value={hashFunction} readOnly />
+            </div>
+
+            <div className="group">
+              <p>Chữ ký</p>
+              <textarea value={signature} readOnly />
+              <div className="gr-btn">
+                <button className="btn1" onClick={convert}>
+                  Chuyển
+                </button>
+                <SaveTextToFile content={signature} />
+              </div>
+            </div>
+          </div>
+
+          <div className="checkSignature">
+            <p className="title">Kiểm tra chữ ký</p>
+
+            <div className="group">
+              <p>Văn bản ký</p>
+              <textarea
+                value={verificationText}
+                onChange={(e) => setVerificationText(e.target.value)}
+              />
+              <FileUploader
+                title="File văn bản"
+                onFileChange={(uploadContent) => {
+                  setVerificationText(uploadContent);
+                }}
+              />
+            </div>
+
+            <div className="bt-sign">
+              <button onClick={handleVerify}>Kiểm tra chữ kí</button>
+            </div>
+            <div className="group">
+              <p>Chữ ký</p>
+              <textarea
+                value={verificationSignature}
+                onChange={(e) => setVerificationSignature(e.target.value)}
+              />
+              <FileUploader
+                title="File chữ ký"
+                onFileChange={(uploadContent) => {
+                  setVerificationSignature(uploadContent);
+                }}
+              />
+            </div>
+
+            <div className="gr-hashFunction">
+              <p>Hàm băm</p>
+              <textarea value={verificationResult} readOnly />
+            </div>
+
+            <div className="gr-hashFunction">
+              <p>Thông báo</p>
+              <div
+                className={`noti ${
+                  notification === "Chữ ký hợp lệ" ? "green" : "red"
+                }`}
+              >
+                {notification}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/rsa.js
@@ -0,0 +1,108 @@
+/* eslint-disable */
+
+class RSA {
+  constructor() {
+    this.TaoKhoa();
+  }
+
+  //hàm kiểm tra số nguyên tố
+  KiemTraNguyenTo(i) {
+    if (i < 2) return false;
+    for (let j = 2; j * j <= i; j++) {
+      if (i % j === 0) return false;
+    }
+    return true;
+  }
+
+  //hàm tạo khóa
+  TaoKhoa() {
+    // hàm random số có 2 chữ số
+    const rd = () => Math.floor(Math.random() * (101 - 11) + 11);
+    do {
+      //random số p có 2 chữ số và kiểm tra xem có phải là số nguyên tố không
+      //tiếp tục random cho đến khi nào p là số nguyên tố thì dừng
+      this.p = rd();
+    } while (!this.KiemTraNguyenTo(this.p));
+
+    do {
+      //random số q có 2 chữ số và kiểm tra xem có phải là số nguyên tố không
+      //tiếp tục random cho đến khi nào q là số nguyên tố thì dừng
+      this.q = rd();
+    } while (!this.KiemTraNguyenTo(this.q));
+
+    //tính n = p * q
+    this.n = this.p * this.q;
+
+    do {
+      //random số e có 2 chữ số và kiểm tra xem có phải là số nguyên tố không, và e phải nguyên tố cùng nhau với (p-1)*(q-1)
+      //tiếp tục random cho đến khi nào e là số nguyên tố và nguyên tố cùng nhau với (p-1)*(q-1) thì dừng
+      this.e = Math.floor(Math.random() * (100 - 2) + 2);
+    } while (
+      !this.KiemTraNguyenTo(this.e) ||
+      !this.NguyenToCungNhau(this.e, (this.p - 1) * (this.q - 1))
+    );
+
+    let k = 1;
+    while (true) {
+      //tìm d sao cho (k*(p-1)*(q-1)+1) chia hết cho e
+      if ((k * (this.p - 1) * (this.q - 1) + 1) % this.e === 0) {
+        this.d = (k * (this.p - 1) * (this.q - 1) + 1) / this.e;
+        break;
+      }
+      k++;
+    }
+  }
+
+  //hàm kiểm tra 2 số có phải là nguyên tố cùng nhau không
+  NguyenToCungNhau(a, b) {
+    //hàm tìm ước chung lớn nhất
+    const GCD = (x, y) => {
+      while (y !== 0) {
+        const temp = y;
+        y = x % y;
+        x = temp;
+      }
+      return x;
+    };
+    //nếu ước chung lớn nhất của 2 số là 1 thì 2 số đó là nguyên tố cùng nhau, return true, ngược lại return false
+    return GCD(a, b) === 1;
+  }
+
+  //hàm tính a^e mod n
+  Mod(m, e, n) {
+    let kq = 1;
+    m = m % n;
+    while (e > 0) {
+      if (e % 2 === 1) {
+        kq = (kq * m) % n;
+      }
+      e = e >> 1;
+      m = (m * m) % n;
+    }
+    return kq;
+  }
+
+  //hàm mã hoá
+  MaHoa(s) {
+    const nguyen = Array.from(s).map((c) => c.charCodeAt(0));
+    const a = nguyen.map((n) => this.Mod(n, this.e, this.n));
+    const str = String.fromCharCode(...a);
+    return btoa(unescape(encodeURIComponent(str)));
+  }
+
+  //hàm giải mã
+  GiaiMa(s) {
+    const giaima = decodeURIComponent(escape(atob(s)));
+    const b = Array.from(giaima).map((c) => c.charCodeAt(0));
+    const c = b.map((n) => this.Mod(n, this.d, this.n));
+    return String.fromCharCode(...c);
+  }
+}
+
+import CryptoJs from "crypto-js";
+//hàm băm md5
+function ComputeMd5Hash(input) {
+  return CryptoJs.MD5(input).toString(CryptoJs.enc.Hex);
+}
+
+export { RSA, ComputeMd5Hash };
